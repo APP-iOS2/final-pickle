690fb5ef0837089fc9179e5be3c68872169121bf
Kai Kim <sdffs310@naver.com>
2023-09-26 16:48:24 +0900

    fix: Calendar 구현 변경
    구현중

--- Pickle/Pickle/Screen/Calendar/CalendarView.swift
+++ Pickle/Pickle/Screen/Calendar/CalendarView.swift
@@ -9,71 +9,193 @@ import SwiftUI
 
 struct CalendarView: View {
     @StateObject var calendarModel: CalendarViewModel = CalendarViewModel()
-    @Namespace var animation
+    @State private var weekSlider: [[Date.WeekDay]] = []
+    @State private var currentWeekIndex: Int = 1
+    @State private var createWeek: Bool = false
+    @State private var currentDate: Date = .init()
+    @Namespace private var animation
     
     var body: some View {
         
         ScrollView(.vertical) {
-            LazyVStack(spacing: 15, pinnedViews: [.sectionHeaders]) {
-                Section {
+//            LazyVStack(spacing: 15, pinnedViews: [.sectionHeaders]) {
+//                Section {
+//                    
+//                    ScrollView(.horizontal, showsIndicators: false) {
+//                        
+//                        HStack(spacing: 10) {
+//                            ForEach(calendarModel.currentWeek, id: \.self) { day in
+//                                
+//                                VStack(spacing: 10) {
+//                                    Text(calendarModel.extractDate(date: day, format: "dd"))
+//                                        .fontWeight(.semibold)
+//                                    Text(calendarModel.extractDate(date: day, format: "EEE"))
+//                                        .fontWeight(.semibold)
+//                                    Circle()
+//                                        .fill(.white)
+//                                        .frame(width: 8, height: 8)
+//                                        .opacity(calendarModel.isToday(date: day) ? 1 : 0)
+//                                }
+//                                .foregroundStyle(calendarModel.isToday(date: day) ? .primary : .tertiary)
+//                                .foregroundColor(calendarModel.isToday(date: day) ? .white : .black)
+//                                .frame(width: 45, height: 90)
+//                                .background(
+//                                    
+//                                    ZStack {
+//                                        
+//                                        if calendarModel.isToday(date: day) {
+//                                            
+//                                            Capsule()
+//                                                .fill(.black)
+//                                                .matchedGeometryEffect(id: "CURRENTDAY", in: animation)
+//                                        }
+//                                    }
+//                                )
+//                                .contentShape(Capsule())
+//                                .onTapGesture {
+//                                    withAnimation {
+//                                        calendarModel.currentDay = day
+//                                    }
+//                                }
+//                                
+//                            }
+//                        }
+//                        .padding(.horizontal)
+//                    }
+//                    TasksView()
+//                    
+//                } header: {
+//                    HeaderView()
+//                        
+//                }
+//                
+//            }
+            
+            TabView(selection: $currentWeekIndex) {
+                ForEach(weekSlider.indices, id: \.self) { index in
+                    let week = weekSlider[index]
+                    WeekView(week)
+                        .padding(.horizontal, 15)
+                        .tag(index)
+                }
+            }
+//            .padding(.horizontal, -15)
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            .frame(height: 100)
+
+//            VStack {
+//                Text("여기 아래로 내일 미션 현황 넣기")
+//            }
+        }
+        .onAppear(perform: {
+            if weekSlider.isEmpty {
+                let currentWeek = Date().fetchWeek()
+                
+                if let firstDate = currentWeek.first?.date {
                     
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        
-                        HStack(spacing: 10) {
-                            ForEach(calendarModel.currentWeek, id: \.self) { day in
-                                
-                                VStack(spacing: 10) {
-                                    Text(calendarModel.extractDate(date: day, format: "dd"))
-                                        .fontWeight(.semibold)
-                                    Text(calendarModel.extractDate(date: day, format: "EEE"))
-                                        .fontWeight(.semibold)
-                                    Circle()
-                                        .fill(.white)
-                                        .frame(width: 8, height: 8)
-                                        .opacity(calendarModel.isToday(date: day) ? 1 : 0)
-                                }
-                                .foregroundStyle(calendarModel.isToday(date: day) ? .primary : .tertiary)
-                                .foregroundColor(calendarModel.isToday(date: day) ? .white : .black)
-                                .frame(width: 45, height: 90)
-                                .background(
-                                    
-                                    ZStack {
-                                        
-                                        if calendarModel.isToday(date: day) {
-                                            
-                                            Capsule()
-                                                .fill(.black)
-                                                .matchedGeometryEffect(id: "CURRENTDAY", in: animation)
-                                        }
-                                    }
-                                )
-                                .contentShape(Capsule())
-                                .onTapGesture {
-                                    withAnimation {
-                                        calendarModel.currentDay = day
-                                    }
-                                }
-                                
+                    weekSlider.append(firstDate.creatPreviousWeek())
+                    
+                }
+                weekSlider.append(currentWeek)
+                
+                if let lastDate = currentWeek.last?.date {
+                    weekSlider.append(lastDate.creatNextWeek())
+                }
+            }
+        })
+       // .ignoresSafeArea(.container, edges: .top)
+        
+    }
+
+    // MARK: - Week View
+    @ViewBuilder
+    func WeekView(_ week: [Date.WeekDay]) -> some View {
+        HStack(spacing: 0) {
+            ForEach(week) { day in
+                VStack(spacing: 8) {
+                    Text(day.date.format("E"))
+                        .font(.callout)
+                        .fontWeight(.medium)
+                        .foregroundColor(.gray)
+                    
+                    Text(day.date.format("dd"))
+                        .font(.callout)
+                        .fontWeight(.bold)
+                        .foregroundStyle(isSameDate(day.date, date2: currentDate) ? .white : .gray)
+                        .frame(width: 35, height: 35)
+                        .background {
+                            
+                            if isSameDate(day.date, date2: currentDate) {
+                                Circle()
+                                    .fill(Color.orange)
+                                    .matchedGeometryEffect(id: "TABINDICATOR", in: animation)
                             }
-                        }
-                        .padding(.horizontal)
-                    }
-                    TasksView()
-                    
-                } header: {
-                    HeaderView()
+                            // MARK: - Indicator to show, which one is Today
+                            if day.date.isToday {
+                                Circle()
+                                    .fill(Color.blue)
+                                    .frame(width: 5, height: 5)
+                                    .offset(y: 30)
+                            }
                         
+                        }
+                        .background(.white.shadow(.drop(radius: 1)), in: .circle)
+                }
+                .hCenter()
+                .contentShape(.rect)
+                .onTapGesture {
+                    
+                    // MARK: - Updating Current Date
+                    withAnimation(.snappy) {
+                        currentDate = day.date
+                    }
                 }
                 
             }
             
-            VStack {
-                Text("여기 아래로 내일 미션 현황 넣기")
+        }
+        .background {
+            GeometryReader {
+                let minX = $0.frame(in: .global).minX
+                
+                Color.clear
+                    .preference(key: OffsetKey.self, value: minX)
+                    .onPreferenceChange(OffsetKey.self) { value in
+                        // MARK: - when the offset reaches 15 and the createweek is toggled then generating next set of week
+                        if value.rounded() == 15 && createWeek {
+                            paginationWeek()
+                            print("Generate")
+                            createWeek =  false
+                        }
+                    }
             }
         }
-        .ignoresSafeArea(.container, edges: .top)
         
     }
+    
+    func paginationWeek() {
+        if weekSlider.indices.contains(currentWeekIndex) {
+            if let firstDate =  weekSlider[currentWeekIndex].first?.date, currentWeekIndex == 0 {
+                
+                
+                // MARK: - inserting new week at 0th index and removing last arry item
+                weekSlider.insert(firstDate.creatPreviousWeek(), at: 0 )
+                weekSlider.removeLast()
+                
+            }
+            
+            if let lastDate = weekSlider[currentWeekIndex].first?.date, currentWeekIndex == (weekSlider.count - 1){
+                
+                // MARK: - Appending new week at last index and removing first arry item
+                weekSlider.append(lastDate.creatNextWeek())
+                weekSlider.removeFirst()
+                
+            }
+                
+        }
+        
+    }
+    
     
     // MARK: - 캘린더 뷰
     func TasksView() -> some View {
@@ -161,6 +283,12 @@ struct CalendarView: View {
             }
             .hLeading()
         }
+        .onChange(of: currentWeekIndex) { newValue in
+            if newValue == 0 || newValue == (weekSlider.count - 1) {
+                createWeek = true
+            }
+        }
+
         .padding()
         .padding(.top, getSafeArea().top)
         .background(Color.white)
@@ -189,6 +317,12 @@ extension View {
             .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .center)
         
     }
+    
+    // MARK: - Checking Two dates are same
+    func isSameDate(_ date1: Date, date2: Date) -> Bool {
+        return Calendar.current.isDate(date1, inSameDayAs: date2)
+    }
+    
     // MARK: - Safe Area
     func getSafeArea() -> UIEdgeInsets {
         guard let screen = UIApplication.shared.connectedScenes.first as? UIWindowScene else {

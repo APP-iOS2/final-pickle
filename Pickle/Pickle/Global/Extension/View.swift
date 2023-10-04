94fd047a6b07ee814c0701ec69bcf9e43887581d
pHH <gudghks56@gmail.com>
2023-10-04 16:39:30 +0900

    feat: TextField, Navigation, Text Modifier 추가

--- Pickle/Pickle/Global/Extension/View.swift
+++ Pickle/Pickle/Global/Extension/View.swift
@@ -10,20 +10,36 @@ import SwiftUI
 extension View {
     /// MainView들을 제외한 View 에서 사용가능한 공통 네비게이션 바 수정자
     func infanNavigationBar(title: String) -> some View {
-        modifier(NavigationBar(title: title))
+        modifier(NavigationBar(title: title,visible: true))
     }
     
-    @ViewBuilder
     func hSpacing(_ alignment: Alignment) -> some View {
         self
             .frame(maxWidth: .infinity, alignment: alignment)
     }
     
-    @ViewBuilder
     func vSpacing(_ alignment: Alignment) -> some View {
         self
             .frame(maxHeight: .infinity, alignment: alignment)
     }
-    
-    
+}
+
+extension View {
+    func backKeyModifier(_ title: String = "", visible: Bool = true) -> some View {
+        modifier(NavigationBar(title: title, visible: visible))
+    }
+}
+
+extension View {
+    func cornerRadiusModifier(frame width: CGFloat = 200,
+                              cornerRadius: CGFloat = 12) -> some View {
+        modifier(CornerButtonModifier(width: width ,
+                                      cornerRadius: cornerRadius))
+    }
+}
+
+extension View {
+    func makeTextField(_ completion: @escaping () -> Void) -> some View {
+        modifier(TextFieldModifier(completion: completion))
+    }
 }

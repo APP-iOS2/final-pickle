f9ad6d3127939573c936d4f0f6622de3e2a6a132
pHH <gudghks56@gmail.com>
2023-10-05 17:23:02 +0900

    feat: BaseRepository 생성: DBStore protocol을 의존성으로 가지고 있다, repository 구현체의 부모 클래스

---
+++ Pickle/Pickle/Data/Repository/Repository/BaseRepository.swift
@@ -0,0 +1,57 @@
+//
+//  BaseRepository.swift
+//  Pickle
+//
+//  Created by 박형환 on 10/4/23.
+//
+
+import Foundation
+import RealmSwift
+
+class BaseRepository<T> {
+    
+    private var dbStore: DBStore
+    
+    required init(dbStore: DBStore) {
+        self.dbStore = dbStore
+    }
+    
+//    required init(dbStore: DBStore = FireBaseStore()) {
+//        self.dbStore = dbStore
+//    }
+    
+    func fetch(_ model: T.Type,
+               predicate: NSPredicate?,
+               sorted: Sorted,
+               completion: ([T]) -> Void) where T: Storable {
+        do {
+            try dbStore.fetch(model,
+                              predicate: predicate,
+                              sorted: sorted,
+                              complection: completion)
+        } catch {
+            return
+        }
+    }
+    
+    func deleteAll(_ model: T.Type) throws where T: Storable {
+        try dbStore.deleteAll(model)
+    }
+    
+    func delete(object: T) throws where T: Storable {
+        try dbStore.delete(object: object)
+    }
+    
+    func update(object: T) throws where T: Storable {
+        try dbStore.update(object: object)
+    }
+    
+    func save(object: T) throws where T: Storable {
+        try dbStore.save(object: object)
+    }
+    
+    func create(_ model: T.Type,
+                completion: @escaping (T) -> Void) throws where T: Storable {
+        try dbStore.create(model, completion: completion)
+    }
+}

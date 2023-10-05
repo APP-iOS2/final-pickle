6f09a26253efc359a158399e3fd0c65673fbaa63
pHH <gudghks56@gmail.com>
2023-10-05 17:21:15 +0900

    feat: MissionRepository 생성

---
+++ Pickle/Pickle/Data/Repository/Repository/MissionRepository.swift
@@ -0,0 +1,13 @@
+//
+//  MissionRepository.swift
+//  Pickle
+//
+//  Created by 박형환 on 10/5/23.
+//
+
+import Foundation
+
+
+final class MissionRepository: BaseRepository<TimeMissionObject> {
+    
+}

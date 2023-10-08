9e62ee6d9871232b0899914f6d8b666b0b1d6982
pHH <gudghks56@gmail.com>
2023-10-08 20:00:22 +0900

    style: 코드 스타일 정렬, lint 규칙 적용

--- Pickle/Pickle/Data/Repository/Protocol/DBStore.swift
+++ Pickle/Pickle/Data/Repository/Protocol/DBStore.swift
@@ -7,12 +7,11 @@
 
 import Foundation
 
-
-//MARK: DB나 persistence 저장소에 저장가능한 타입을 나타내는 Protocol
+// MARK: DB나 persistence 저장소에 저장가능한 타입을 나타내는 Protocol
 protocol Storable {}
 
-//MARK: 프로퍼티 래퍼로 의존성 주입을 하기위한 Protocol,
-//MARK: @Injected로 주입하기 위해서는 Dependency Protocol을 만족하도록 해야한다.
+// MARK: 프로퍼티 래퍼로 의존성 주입을 하기위한 Protocol,
+// MARK: @Injected로 주입하기 위해서는 Dependency Protocol을 만족하도록 해야한다.
 protocol Dependency {}
 
 protocol DBStore: Dependency {
@@ -23,3 +22,5 @@ protocol DBStore: Dependency {
     func deleteAll<T: Storable>(_ model: T.Type) throws
     func fetch<T: Storable>(_ model: T.Type, predicate: NSPredicate?, sorted: Sorted?, complection: ([T]) -> Void) throws
 }
+
+

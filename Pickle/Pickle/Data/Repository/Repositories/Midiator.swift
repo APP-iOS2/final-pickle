be7274e9803f7d8ae0e8759ca3c09bb6ccedaaf5
pHH <gudghks56@gmail.com>
2023-10-08 19:56:13 +0900

    feat: 임시 mediator

---
+++ Pickle/Pickle/Data/Repository/Repositories/Midiator.swift
@@ -0,0 +1,82 @@
+//
+//  Adapter.swift
+//  Pickle
+//
+//  Created by 박형환 on 10/7/23.
+//
+
+import Foundation
+
+
+//protocol MissionRepositoryProtocol: Dependency {
+//    associatedtype DTO: Mission
+//    associatedtype Persited: MissionObject
+//
+//}
+enum Adpater {
+    static weak var repository: (any MissionRepositoryProtocol)?
+    static var type: MissionRepositoryType = .none
+}
+
+enum MissionError: Error {
+    case castingError
+}
+
+struct MissionMediator {
+    @Injected(TimeRepoKey.self) var timeRespository: any TimeRepositoryProtocol
+    @Injected(BehaviorRepoKey.self) var behaviorRepository: any BehaviorRepositoryProtocol
+    
+    typealias DTO = Mission
+    typealias Persisted = MissionObject
+    typealias A = Adpater
+    
+    private func setRepository(mission: MissionType) {
+        let missionType = mission.type
+        if Adpater.type == missionType { return }
+        else { Adpater.type = missionType }
+        
+        switch missionType {
+        case .time:
+            Adpater.repository = timeRespository
+        case .behavior:
+            Adpater.repository = behaviorRepository
+        default:
+            return
+        }
+    }
+    
+    func fetch<T: Persisted>(sorted: Sorted, type: MissionType) async throws -> T {
+        setRepository(mission: type)
+        let value = await A.repository?.fetch(sorted: sorted)
+        guard let value = value as? T
+        else { throw MissionError.castingError }
+        return value
+    }
+    
+    func create<T: Persisted>(type: MissionType, _ completion: @escaping (T) -> Void) {
+        A.repository?.create { value in
+            Log.debug("create: \(value)")
+        }
+    }
+    
+    func add(type: MissionType, value: some Mission) {
+        setRepository(mission: type)
+        A.repository?.save(model: value)
+    }
+    
+    func delete<T: DTO>(type: MissionType, value: T) where T: MappableProtocol {
+        setRepository(mission: type)
+        if let repo = A.repository {
+//            repo.delete(model: value)
+        }
+    }
+    //    func fetch(sorted: Sorted) async -> [DTO]
+    //    func create(_ completion: @escaping (Persited) -> Void)
+    //    func save(model: DTO)
+    //    func delete(model: DTO)
+    //    func deleteAll()
+    //    func update(model: DTO)
+}
+
+
+

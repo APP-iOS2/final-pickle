3decf503fe832093ccd92dfd01c1e8277d14252b
pHH <gudghks56@gmail.com>
2023-10-08 20:08:22 +0900

    fix: contentView #previews로 변경

--- Pickle/Pickle/Screen/App/ContentView.swift
+++ Pickle/Pickle/Screen/App/ContentView.swift
@@ -34,13 +34,9 @@ struct ContentView: View {
                     .environment(\.symbolVariants, .none)
             }.tag(2)
         }
-        
-
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+#Preview {
+    ContentView()
 }

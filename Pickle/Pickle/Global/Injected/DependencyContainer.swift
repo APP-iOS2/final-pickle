cec4d2c701b27eddea958bb37bfbdece211b6cfc
pHH <gudghks56@gmail.com>
2023-10-08 20:04:21 +0900

    fix: InjectionKey 추가에 따른 Container ,wrapper 리팩토링

--- Pickle/Pickle/Global/Injected/DependencyContainer.swift
+++ Pickle/Pickle/Global/Injected/DependencyContainer.swift
@@ -12,14 +12,31 @@ struct DependencyContainer {
     private static var factories: [String: () -> Any] = [:]
     private static var cache: [String: Any] = [:]
     
+    // MARK: Old version
     static func register<Dependency>(type: Dependency.Type,
                                      _ factory: @autoclosure @escaping () -> Dependency) {
         factories[String(describing: type.self)] = factory
     }
     
+    // MARK: New One using injectionKey
+    static func register<T: InjectionKey>(_ name: T.Type,
+                                          _ factory: @autoclosure @escaping () -> Dependency) {
+        factories[String(describing: name.self)] = factory
+    }
+    
+    //    static func resolve<T>(for type: Any.Type?) -> T {
+    //        let name = type.map { String(describing: $0) } ?? String(describing: T.self)
+    //
+    //        guard let component: T = .modules[name]?.resolve() as? T else {
+    //            fatalError("Dependency '\(T.self)' not resolved!")
+    //        }
+    //
+    //        return component
+    //    }
+    
     static func resolve<Dependency>(_ resolveType: DependencyType = .automatic,
-                                    _ type: Dependency.Type) -> Dependency? {
-        let serviceName = String(describing: type.self)
+                                    _ type: Any.Type?) -> Dependency {
+        let serviceName = type.map { String(describing: $0) } ?? String(describing: Dependency.self)
         
         switch resolveType {
         case .singleton:
@@ -29,19 +46,24 @@ struct DependencyContainer {
                 let service = factories[serviceName]?() as? Dependency
                 if let service = service {
                     cache[serviceName] = service
+                    return service
                 }
-                return service
             }
+            assert(false, "fattalError singleton Occur")
         case .newSingleton:
             let service = factories[serviceName]?() as? Dependency
             if let service = service {
                 cache[serviceName] = service
+                return service
             }
-            return service
+            assert(false, "fattalError singleton Occur")
         case .automatic:
             fallthrough
         case .new:
-            return factories[serviceName]?() as? Dependency
+            if let dependency = factories[serviceName]?() as? Dependency {
+                return dependency
+            }
+            assert(false, "fattalError singleton Occur")
         }
     }
 }
--- Pickle/Pickle/Global/Injected/Injected+Wrapper.swift
+++ Pickle/Pickle/Global/Injected/Injected+Wrapper.swift
@@ -9,17 +9,28 @@ import Foundation
 
 @propertyWrapper
 struct Injected<Dependency> {
-
+    
     var service: Dependency
-
-    init(_ type: DependencyType = .automatic) {
-        guard let service: Dependency = DependencyContainer.resolve(type, Dependency.self) else {
-            let serviceName = String(describing: Dependency.self)
-            fatalError("No service of type \(serviceName) registered!")
-        }
+    
+    // MARK: Before
+    //    init(_ type: DependencyType = .automatic) {
+    //        guard let service: Dependency = DependencyContainer.resolve(type, Dependency.self) else {
+    //            let serviceName = String(describing: Dependency.self)
+    //            fatalError("No service of type \(serviceName) registered!")
+    //        }
+    //        self.service = service
+    //    }
+    
+    // MARK: After
+    init<T>(_ key: T.Type, _ type: DependencyType = .automatic) where T: InjectionKey, Dependency == T.Value {
+        let service: Dependency = key.currentValue
+        //        guard let service: Dependency = DependencyContainer.resolve(type, key.self) else {
+        //            let serviceName = String(describing: Dependency.self)
+        //            fatalError("No service of type \(serviceName) registered!")
+        //        }
         self.service = service
     }
-
+    
     var wrappedValue: Dependency {
         get { self.service }
         mutating set { service = newValue }

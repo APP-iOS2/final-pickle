795a54f13961a093543f1241f29deed70a32bc9e
pHH <gudghks56@gmail.com>
2023-10-09 18:50:29 +0900

    move: MissionType 파일 분리

--- Pickle/Pickle/Screen/ViewModel/MissionType.swift
+++ Pickle/Pickle/Screen/ViewModel/MissionType.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+enum MissionType {
+    case time(TimeMission)
+    case behavior(BehaviorMission)
+    
+    var anyMission: any Mission {
+        switch self {
+        case .time(let timeMission):
+            return timeMission
+        case .behavior(let behaviorMission):
+            return behaviorMission
+        }
+    }
+    
+    var type: MissionRepositoryType {
+        switch self {
+        case .time(_):
+            return .time
+        case .behavior(_):
+            return .behavior
+        }
+    }
+}
+
+enum MissionRepositoryType {
+    case time
+    case behavior
+    case none
+}

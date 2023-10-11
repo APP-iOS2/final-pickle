ca57af4768e1b4e544a4a7ba95440c2d4006a411
pHH <gudghks56@gmail.com>
2023-10-11 09:18:26 +0900

    feat: enum error case 분리

---
+++ Pickle/Pickle/Data/Repository/Realm/Type/PersistentedError.swift
@@ -0,0 +1,15 @@
+//
+//  PersistentedError.swift
+//  Pickle
+//
+//  Created by 박형환 on 10/10/23.
+//
+
+import Foundation
+
+
+enum PersistentedError: Error {
+    case fetchNothing
+    case addFaild
+    case updateFaild
+}

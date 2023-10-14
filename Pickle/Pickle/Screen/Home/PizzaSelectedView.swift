2ddcb22c2dfd103c1055482bf6c8ba08f0c9ff1e
pHH <gudghks56@gmail.com>
2023-10-14 20:01:58 +0900

    style: Icon 싱글 사이즈 셋팅 , lock.fill 아이콘 추가

---
+++ Pickle/Pickle/Screen/Home/PizzaSelectedView.swift
@@ -0,0 +1,8 @@
+//
+//  PizzaSelectedView.swift
+//  Pickle
+//
+//  Created by 박형환 on 10/14/23.
+//
+
+import Foundation
---
+++ Pickle/Pickle/Screen/ViewModel/UserError.swift
@@ -0,0 +1,13 @@
+//
+//  UserError.swift
+//  Pickle
+//
+//  Created by 박형환 on 10/14/23.
+//
+
+import Foundation
+
+
+enum UserError: Error {
+    case pizzsliceAddError
+}

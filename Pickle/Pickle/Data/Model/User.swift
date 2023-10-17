62e215212ff649fbae0abf68208c83c98803d262
pHH <gudghks56@gmail.com>
2023-10-17 11:29:46 +0900

    feat: User 피자를 추가할때 8조각 유효성 처리 함수 추가, 프리뷰용 변수 추가, Equatable

--- Pickle/Pickle/Data/Model/User.swift
+++ Pickle/Pickle/Data/Model/User.swift
@@ -13,8 +13,34 @@ struct User {
     var currentPizzaCount: Int
     var currentPizzaSlice: Int
     var createdAt: Date  // 유저 계정 생성 날짜,시간
+    
+    init(id: String, nickName: String, currentPizzaCount: Int, currentPizzaSlice: Int, createdAt: Date) {
+        self.id = id
+        self.nickName = nickName
+        self.currentPizzaCount = currentPizzaCount
+        self.currentPizzaSlice = currentPizzaSlice
+        self.createdAt = createdAt
+    }
+    
+    /// 피자 조각을 추가하고 , 8조각 이상일시 슬라이스를 0으로 만들고 피자카운트를 (1 = defaut) 증가 시키는 메서드
+    /// - Parameter count: 피자 조각갯수
+    mutating func addPizzaSliceValidation(count: Int = 1) -> Self {
+        currentPizzaSlice += count
+        if currentPizzaSlice >= 8 {
+            currentPizzaSlice = 0
+            currentPizzaCount += 1
+        }
+        return self
+    }
 }
 
+extension User {
+    static let defaultUser: User = .init(id: UUID().uuidString,
+                                         nickName: "Guest",
+                                         currentPizzaCount: 0,
+                                         currentPizzaSlice: 0,
+                                         createdAt: Date())
+}
 /*
  - 포인트
  - 통계
@@ -24,7 +50,7 @@ extension User: MappableProtocol {
     typealias PersistenceType = UserObject
     
     func mapToPersistenceObject() -> PersistenceType {
-        if let id = UUID(uuidString: self.id) {
+        if let _ = UUID(uuidString: self.id) {
             return UserObject(nickName: self.nickName,
                               currentPizzaCount: self.currentPizzaCount,
                               currentPizzaSlice: self.currentPizzaSlice,
@@ -45,5 +71,8 @@ extension User: MappableProtocol {
              currentPizzaSlice: object.currentPizzaSlice,
              createdAt: object.createdAt)
     }
+}
+
+extension User: Equatable {
     
 }

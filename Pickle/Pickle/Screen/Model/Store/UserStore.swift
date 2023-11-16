bbc0263081dbc19c8f2a15ea4c6d7cce8216b67c
pHH <gudghks56@gmail.com>
2023-11-16 09:54:50 +0900

    feat: UserStore 피자 선택 Action case 추가 (특정 액션에 따른 메소드 호출 구조 추가) -#175

--- Pickle/Pickle/Screen/Model/Store/UserStore.swift
+++ Pickle/Pickle/Screen/Model/Store/UserStore.swift
@@ -6,27 +6,53 @@
 //
 
 import SwiftUI
+import Combine
 
 final class UserStore: ObservableObject {
     
     @Injected(UserRepoKey.self) var userRepository: UserRepositoryProtocol
     
     @Published var user: User = User.defaultUser
+    @Published var currentPizza: CurrentPizza = .init(pizza: nil)
     
     var token: RNotificationToken?
     
+    private var subscriptions = Set<AnyCancellable>()
+    
     var pizzaSlice: Double {
-        Double(user.currentPizzaSlice)
+        return Double(user.currentPizzas.map(\.currentPizzaSlice).reduce(0, +))
     }
     
+    // TODO: 변경 필요 - currentPizza로 변경
+    // 기존 user안에있는 slice와 count를 currentPizza 데이터 내부로 이동
     var pizzaCount: Int {
-        user.currentPizzaCount
+        user.currentPizzas.map(\.currentPizzaCount).reduce(0, +)
+    }
+    
+    enum Action {
+        case select(Pizza)
+        case create
+        case delete
+    }
+    
+    func trigger(action: Action) {
+        switch action {
+        case .select(let pizza):
+            Log.debug("pizza")
+            self.selectCurrentPizza(pizza: pizza)
+        case .create:
+            break
+        case .delete:
+            break
+        }
     }
     
     @MainActor
     func fetchUser() throws {
         do {
             self.user = try userRepository.fetchUser()
+            // TODO: 변경 필요
+            self.currentPizza = user.currentPizzas.filter { $0.pizza!.id == user.pizzaID }.first ?? self.user.currentPizzas.first!
             self.token?.invalidate()
             observeUser()
         } catch {
@@ -39,24 +65,27 @@ final class UserStore: ObservableObject {
         self.token?.invalidate()
     }
     
-    @MainActor
-    private func observeUser() {   
-        self.token = userRepository.observeUser(id: self.user.id,
-                                                keyPaths: [\.currentPizzaSlice, \.pizza]) {
-            change in
-            switch change {
-            case .change(let userObject, let propertys):
-                let user = User.mapFromPersistenceObject(userObject)
-                if self.user == user {
-                    Log.error("self.user == user")
-                } else {
-                    Log.error("self.user != user")
-                }
-                return
-            default:
-                break
+    func selectCurrentPizza(pizza: Pizza) {
+        user.updatePublihser(path: \.pizzaID, to: pizza.id)
+            .throttle(for: 0.3, scheduler: DispatchQueue.main, latest: false)
+            .withUnretained(self)
+            .map { store, model -> AnyPublisher<User, Never> in
+                store.userRepository.update(seleted: model)
+                    .replaceError(with: store.user)
+                    .eraseToAnyPublisher()
             }
-        }
+            .switchToLatest()
+            .sinkToResult(with: self) { store, result in
+                switch result {
+                case .success(let success):
+                    store.user = success
+                    store.currentPizza  = success.currentPizzas.filter { $0.pizza!.id == success.pizzaID }.first!
+                    store.subscriptions.removeAll()
+                case .failure(let failure):
+                    Log.error("error occur : \(failure)")
+                }
+            }
+            .store(in: &subscriptions)
     }
     
     func addUser(default user: User = User.defaultUser) {
@@ -70,10 +99,13 @@ final class UserStore: ObservableObject {
     }
     
     func addPizzaSlice(slice count: Int) throws {
-        let user = self.user.addPizzaSliceValidation(count: count)
+        // let user = self.user.addPizzaSliceValidation(count: count)
+        let currentPizza = self.currentPizza.addPizzaSliceValidation()
+        Log.debug("add Pizza Slice: \(currentPizza.currentPizzaSlice)")
+        let user = self.user.update(current: currentPizza)
         do {
             try userRepository.updateUser(model: user)
-            // self.user = user
+            self.user = user
         } catch {
             assert(false)
         }
@@ -105,5 +137,27 @@ final class UserStore: ObservableObject {
         } catch {
             Log.error("\(error)")
         }
+    }
+    
+    @MainActor
+    private func observeUser() {
+        // TODO: 변경 필요
+        // 필요가 있나?
+//        self.token = userRepository.observeUser(id: self.user.id,
+//                                                keyPaths: [\.currentPizzaSlice, \.pizza]) {
+//            change in
+//            switch change {
+//            case .change(let userObject, let propertys):
+//                let user = User.mapFromPersistenceObject(userObject)
+//                if self.user == user {
+//                    Log.error("self.user == user")
+//                } else {
+//                    Log.error("self.user != user")
+//                }
+//                return
+//            default:
+//                break
+//            }
+//        }
     }
 }

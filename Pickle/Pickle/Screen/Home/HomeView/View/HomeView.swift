af486e5464288468003ee210115b1e23a40688f2
pHH <gudghks56@gmail.com>
2023-12-04 16:36:14 +0900

    test github action

--- Pickle/Pickle/Screen/Home/HomeView/View/HomeView.swift
+++ Pickle/Pickle/Screen/Home/HomeView/View/HomeView.swift
@@ -51,7 +51,7 @@ struct HomeView: View {
         content
             .task {
                 await todoStore.fetch()  // MARK: Persistent 저장소에서 Todo 데이터 가져오기
-                
+                Log.debug("testFlight Test Log")
                 if isRunTimer { ongoingTodo = todoStore.getSeletedTodo(id: todoId) }
             }
             .onReceive(userStore.$user) {

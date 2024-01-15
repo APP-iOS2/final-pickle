e5b26631183774fac9095b469d57d38173250440
pHH <gudghks56@gmail.com>
2024-01-16 02:32:08 +0900

    refactor: backGround Schedule Task 코드 삭제

--- Pickle/Pickle/Screen/App/PickleApp.swift
+++ Pickle/Pickle/Screen/App/PickleApp.swift
@@ -20,77 +20,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         PickleApp.setUpDependency()
         let _ = RealmMigrator()
         
-        // App Refresh Task
-        BGTaskScheduler.shared.register(forTaskWithIdentifier: "com.ddudios.realpizza.refresh_badge", using: nil) { task in
-            self.handleAppRefresh(task: task as! BGAppRefreshTask)
-        }
-        
-        // Processing Task
-        BGTaskScheduler.shared.register(forTaskWithIdentifier: "com.ddudios.realpizza.refresh_process", using: nil) { task in
-            self.handleProcessingTask(task: task as! BGProcessingTask) // 타입 캐스팅 유의 (BG'Processing'Task)
-        }
-        
         return true
-    }
-    
-    func handleAppRefresh(task: BGAppRefreshTask) {
-        // 다음 동작 수행, 반복시 필요
-        scheduleAppRefresh()
-        
-        task.expirationHandler = {
-            task.setTaskCompleted(success: false)
-        }
-        
-        // 가벼운 백그라운드 작업 작성
-        task.setTaskCompleted(success: false)
-    }
-    
-    func handleProcessingTask(task: BGProcessingTask) {
-        task.expirationHandler = {
-            task.setTaskCompleted(success: false)
-        }
-        
-        // 무거운 백그라운드 작업 작성
-        task.setTaskCompleted(success: true)
-    }
-    
-    func scheduleAppRefresh() {
-        let request = BGAppRefreshTaskRequest(identifier: "com.ddudios.realpizza.refresh_badge")
-        
-        do {
-            try BGTaskScheduler.shared.submit(request)
-        } catch {
-            Log.error("\(Date()): Could not schedule app refresh: \(error)")
-        }
-    }
-    
-    func scheduleProcessingTaskIfNeeded() {
-        
-        let request = BGProcessingTaskRequest(identifier: "com.ddudios.realpizza.refresh_process")
-        request.requiresExternalPower = false
-        request.requiresNetworkConnectivity = false
-        
-        do {
-            try BGTaskScheduler.shared.submit(request)
-        } catch {
-            Log.error("\(Date()): Could not schedule processing task: \(error)")
-        }
-        
-        guard missionStore.timeMissions[0].date.format("yyyy-MM-dd") != Date().format("yyyy-MM-dd") else { return }
-        
-        BGTaskScheduler.shared.register(forTaskWithIdentifier: "com.ddudios.realpizza.refresh_process", using: nil) { task in
-            self.handleProcessingTask(task: task as! BGProcessingTask)
-        }
-    }
-    
-    func application(_ application: UIApplication,
-                     configurationForConnecting connectingSceneSession: UISceneSession,
-                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        let sceneConfiguration = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
-        
-        sceneConfiguration.delegateClass = MySceneDelegate.self
-        
-        return sceneConfiguration
     }
 }
 
@@ -129,7 +59,7 @@ struct PickleApp: App {
                 Text("value ")
             } else {
                 ContentView()
-                    .onAppear { Log.error("contentVIew onAppear"); debugDelete.toggle() }    // 내부의 contentView onApper 보다 늦게 실행됨 Debug Delete
+                    .onAppear {  debugDelete.toggle() }    // 내부의 contentView onApper 보다 늦게 실행됨 Debug Delete
                     .environmentObject(todoStore)
                     .environmentObject(missionStore)
                     .environmentObject(userStore)
@@ -148,7 +78,6 @@ struct PickleApp: App {
     private func backgroundEvent(newScene: ScenePhase) {
 
         if newScene == .background {
-            
             backgroundNumber += 1
             
             timerVM.activeNumber += 1
@@ -161,32 +90,24 @@ struct PickleApp: App {
                 timerVM.backgroundSpendTime = timerVM.spendTime
                 timerVM.backgroundTimeExtra = timerVM.timeExtra
             }
-            
         }
+        
         if newScene == .active {
-            #if DEBUG
-            print("ACTIVE")
-            
-            print("activeNumber: \(timerVM.activeNumber)")
-            print("backgroundNumber: \(backgroundNumber)")
-            print("isRunTimer: \(isRunTimer)")
-            #endif
+            Log.debug("ACTIVE")
+            Log.debug("activeNumber: \(timerVM.activeNumber)")
+            Log.debug("backgroundNumber: \(backgroundNumber)")
+            Log.debug("isRunTimer: \(isRunTimer)")
             
             if isRunTimer {
-                
                 if timerVM.activeNumber != backgroundNumber {
-                    
                     timerVM.todo = todoStore.getSeletedTodo(id: todoId)
                     timerVM.showOngoingAlert = true
-                
                 }
                 
                 if timerVM.fromBackground {
-                    
                     timerVM.makeRandomSaying()
                     var currentTime: Date = Date()
                     var diff = currentTime.timeIntervalSince(timerVM.backgroundTimeStemp)
-                    
                     timerVM.timeRemaining = timerVM.backgroundTimeRemain
                     timerVM.spendTime = timerVM.backgroundSpendTime
                     timerVM.spendTime += diff

36cbd185a4645207e08b073448f29db20cfdf2da
pHH <gudghks56@gmail.com>
2024-01-16 02:31:23 +0900

    안쓰는 코드 삭제

--- Pickle/Pickle/Screen/App/MySceneDelegate.swift
+++ Pickle/Pickle/Screen/App/MySceneDelegate.swift
@@ -8,13 +8,3 @@
 import Foundation
 import UIKit
 
-class MySceneDelegate: UIResponder, UIWindowSceneDelegate {
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
-            appDelegate.scheduleAppRefresh()
-            appDelegate.scheduleProcessingTaskIfNeeded()
-        } else {
-            Log.error("AppDelegate를 찾을 수 없습니다.")
-        }
-    }
-}
